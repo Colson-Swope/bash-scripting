25dec6949037d2793c7b170741bee522c53eda8c
Colson-Swope <colsonjswope@gmail.com>
2024-01-24 10:04:19 -0800

    pseudo code implemented

--- log_inspector.sh
+++ log_inspector.sh
@@ -2,8 +2,13 @@
 
 # Write a script that inspects a log file 
 
+# use the lastlog file on the WWU lab machines 
+# this file is located in /var/log
+# the overall goal is to iterate though this log file and determine useful info based on what is found in the log file 
+# Our goal: how many times did user X log in between January and December 
 
+# two cmd args: target user and target date range (in months) 
 
-# Write a script that inspects a log file 
+# must include one function (what do I want this function to be?) 
 
 
